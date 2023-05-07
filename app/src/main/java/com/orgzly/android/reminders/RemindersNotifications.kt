b138c49585574224d8e1fde4f81f2f028884fc24
Marek G <marek-dev@yandex.com>
2023-05-07 15:22:19 +0200

    Add setting for making reminder notifications sticky (default)

--- app/src/main/java/com/orgzly/android/reminders/RemindersNotifications.kt
+++ app/src/main/java/com/orgzly/android/reminders/RemindersNotifications.kt
@@ -54,6 +54,12 @@ object RemindersNotifications {
                 builder.setVibrate(VIBRATION_PATTERN)
             }
 
+            // Set notification sticky
+            if (AppPreferences.remindersSticky(context)) {
+                builder.setOngoing(true)
+                builder.setAutoCancel(false)
+            }
+
             // Set notification sound
             if (AppPreferences.remindersSound(context)) {
                 builder.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
